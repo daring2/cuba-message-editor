ce36887c60a988d4b5828e544e65829c22def798
daring <danila.daring@gmail.com>
2021-09-17 12:34:40 +0300

    #1 - refactoring

--- modules/web/src/ru/itsyn/cuba/message_editor/web/screens/message_entity/MessageEntityEditor.java
+++ modules/web/src/ru/itsyn/cuba/message_editor/web/screens/message_entity/MessageEntityEditor.java
@@ -1,6 +1,7 @@
 package ru.itsyn.cuba.message_editor.web.screens.message_entity;
 
-import com.haulmont.cuba.core.global.LocaleResolver;
+import com.haulmont.cuba.core.global.GlobalConfig;
+import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.components.TextArea;
 import com.haulmont.cuba.gui.model.InstanceContainer.ItemPropertyChangeEvent;
 import com.haulmont.cuba.gui.screen.*;
@@ -8,6 +9,7 @@ import ru.itsyn.cuba.message_editor.entity.MessageEntity;
 import ru.itsyn.cuba.message_editor.web.screens.util.MessageEntityHelper;
 
 import javax.inject.Inject;
+import java.util.LinkedHashMap;
 
 @UiController("msg_MessageEntity.edit")
 @UiDescriptor("message-entity-editor.xml")
@@ -16,9 +18,26 @@ import javax.inject.Inject;
 public class MessageEntityEditor extends StandardEditor<MessageEntity> {
 
     @Inject
+    protected GlobalConfig globalConfig;
+    @Inject
     protected MessageEntityHelper messageEntityHelper;
     @Inject
+    protected LookupField<String> localeField;
+    @Inject
     protected TextArea<String> defaultTextField;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        initLocaleField();
+    }
+
+    protected void initLocaleField() {
+        var opts = new LinkedHashMap<String, String>();
+        globalConfig.getAvailableLocales().forEach((name, locale) ->
+                opts.put(name, locale.getLanguage())
+        );
+        localeField.setOptionsMap(opts);
+    }
 
     @Subscribe
     public void onBeforeShow(BeforeShowEvent event) {
